5383f44a2d5ff7681d5c1e2fd650ea91512c5970
LimeLicker8348 <157431518+LimeLicker8348@users.noreply.github.com>
2024-01-25 11:13:15 -0500

    Update addTokens.jskpng.54

--- global/addTokens.js
+++ global/addTokens.js
@@ -50,7 +50,7 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
+                            addedTokens: 69696969,
                             addedXp: 300
                         }, e.secret)
                     }).then(() => alert('Added daily rewards!')).catch(() => alert('There was an error when adding rewards!'));;
