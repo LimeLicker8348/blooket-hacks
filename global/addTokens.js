201163b940a3df89c84f17612f60d0a19289c84e
LimeLicker8348 <157431518+LimeLicker8348@users.noreply.github.com>
2024-01-30 12:32:47 -0500

    Update addTokens.jsm.t5

--- global/addTokens.js
+++ global/addTokens.js
@@ -37,7 +37,7 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
+                            addedTokens: 40000,
                             addedXp: 300
                         }, e.secret)
                     });
